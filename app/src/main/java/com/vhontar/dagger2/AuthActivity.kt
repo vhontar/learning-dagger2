5ecc90f49a6634ff4bae36397371799f2081b9c1
Vladyslav Hontar <vlad.hontar@techs.com.ua>
2020-07-13 12:52:12 +0300

    add some new dependencies and create providers for glide to have an instance in every activity to load images

--- app/src/main/java/com/vhontar/dagger2/AuthActivity.kt
+++ app/src/main/java/com/vhontar/dagger2/AuthActivity.kt
@@ -1,11 +1,28 @@
 package com.vhontar.dagger2
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.RequestManager
+import com.vhontar.dagger2.databinding.ActivityAuthBinding
 import dagger.android.support.DaggerAppCompatActivity
+import javax.inject.Inject
 
 class AuthActivity : DaggerAppCompatActivity() {
+
+    private lateinit var viewDataBinding: ActivityAuthBinding
+
+    @Inject
+    lateinit var logo: Drawable
+
+    @Inject
+    lateinit var requestManager: RequestManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
+        viewDataBinding = DataBindingUtil.setContentView(this, R.layout.activity_auth)
+        requestManager
+            .load(logo)
+            .into(viewDataBinding.loginLogo)
     }
 }
--- app/src/main/java/com/vhontar/dagger2/di/AppModule.kt
+++ app/src/main/java/com/vhontar/dagger2/di/AppModule.kt
@@ -1,6 +1,14 @@
 package com.vhontar.dagger2.di
 
+import android.app.Application
+import android.graphics.drawable.Drawable
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestManager
+import com.bumptech.glide.request.RequestOptions
+import com.vhontar.dagger2.R
 import dagger.Module
+import dagger.Provides
 
 /**
  * Created by Vladyslav Hontar (vhontar) on 13.07.20.
@@ -8,4 +16,24 @@ import dagger.Module
 
 @Module
 class AppModule {
+
+    companion object {
+        @Provides
+        fun provideRequestOptions(): RequestOptions {
+            return RequestOptions
+                .placeholderOf(R.drawable.white_background)
+                .error(R.drawable.white_background)
+        }
+
+        @Provides
+        fun provideGlideInstance(application: Application, requestOptions: RequestOptions): RequestManager {
+            return Glide.with(application)
+                .setDefaultRequestOptions(requestOptions)
+        }
+
+        @Provides
+        fun provideAppDrawable(application: Application): Drawable {
+            return ContextCompat.getDrawable(application, R.drawable.logo)!!
+        }
+    }
 }
